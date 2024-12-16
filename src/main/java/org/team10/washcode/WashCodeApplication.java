38ccc70fdd3ca3625053363ea1d47e655468f42d
HYEONSIK <ks06891@naver.com>
2024-12-16 11:45:53 +0900

    Feat: Spring Boot 프로젝트 파일 추가

---
+++ src/main/java/org/team10/washcode/WashCodeApplication.java
@@ -0,0 +1,13 @@
+package org.team10.washcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WashCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/team10/washcode/WashCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.team10.washcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
