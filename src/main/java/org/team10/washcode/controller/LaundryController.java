6fc621c72bbe16185cd67801820e13efe57e0452
진세현 <niki8533@naver.com>
2024-12-18 18:15:59 +0900

    Feat: 세탁소 정보 등록하기 #9

--- src/main/java/org/team10/washcode/controller/LaundryController.java
+++ src/main/java/org/team10/washcode/controller/LaundryController.java
@@ -1,9 +1,16 @@
 package org.team10.washcode.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class LaundryController {
+    @RequestMapping("/laundry")
+    public String laundry(HttpServletRequest request) {
+        System.out.println(request.getParameter("address"));
+
+        return "register-shop";
+    }
 }
--- src/main/java/org/team10/washcode/controller/PageController.java
+++ src/main/java/org/team10/washcode/controller/PageController.java
@@ -1,10 +1,17 @@
 package org.team10.washcode.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
     public String index() {
         return "main";
     }
+
+    @RequestMapping("register-shop")
+    public String registerShop() {
+        return "Shop/register-shop";
+    }
+
 }
