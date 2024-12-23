efb3a0408f21ff5cb706155980b2acdca40abda3
이종찬 <ljch1104@gmail.com>
2024-12-23 11:36:06 +0900

    [feat] 회원정보 조회, 수정, 삭제 기능 개발 #18

--- src/main/java/org/team10/washcode/repository/UserRepository.java
+++ src/main/java/org/team10/washcode/repository/UserRepository.java
@@ -8,15 +8,18 @@ import org.team10.washcode.ResponseDTO.user.UserProfileResDTO;
 import org.team10.washcode.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("select count(*) from User u where u.email = :email")
-    int findByEmailExists(@Param("email") String email);
+    @Query("SELECT EXISTS (SELECT true FROM User u where u.email = :email)")
+    Boolean findByEmailExists(@Param("email") String email);
 
-    @Query("select count(*) from User u where u.email = :email and u.password = :password")
-    int findByPasswordEquals(@Param("email") String email, @Param("password") String password);
+    @Query("SELECT EXISTS (SELECT true FROM User u where u.email = :email and u.password = :password)")
+    Boolean findByPasswordEquals(@Param("email") String email, @Param("password") String password);
 
-    @Query("select new org.team10.washcode.ResponseDTO.user.UserProfileResDTO(u.name, u.address, u.phone) from User u where u.id = :id")
-    UserProfileResDTO findUserProfileById(@Param("id") int id);
+    //@Query("select new org.team10.washcode.ResponseDTO.user.UserProfileResDTO(u.name, u.address, u.phone) from User u where u.id = :id")
+    //UserProfileResDTO findUserProfileById(@Param("id") int id);
+
+    Optional<User> findById(@Param("id") int id);
 }
