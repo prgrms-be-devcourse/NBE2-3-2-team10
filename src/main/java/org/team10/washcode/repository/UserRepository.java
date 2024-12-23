94838e4b4cbd7ca13e8fd6ddbe35fbebe88468fd
이종찬 <ljch1104@gmail.com>
2024-12-23 12:11:31 +0900

    [fix] 오류수정 #18

--- src/main/java/org/team10/washcode/repository/UserRepository.java
+++ src/main/java/org/team10/washcode/repository/UserRepository.java
@@ -20,11 +20,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT EXISTS (SELECT true FROM User u where u.email = :email and u.password = :password)")
     Boolean findByPasswordEquals(@Param("email") String email, @Param("password") String password);
 
-
-    @Query("select count(*) from User u where u.email = :email")
-    int findByEmailExists(@Param("email") String email);
-
-
     //@Query("select new org.team10.washcode.ResponseDTO.user.UserProfileResDTO(u.name, u.address, u.phone) from User u where u.id = :id")
     //UserProfileResDTO findUserProfileById(@Param("id") int id);
 
